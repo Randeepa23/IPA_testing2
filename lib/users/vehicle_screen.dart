41537575f2d4a291303bced8485a35d0c59677cd
Induru Udantha <Induruu@Exploreholding.com>
2026-02-20 14:17:10 +0530

    Update

--- lib/users/vehicle_screen.dart
+++ lib/users/vehicle_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../Vehicle/vehicle_new_request_screen.dart';
 import '../Vehicle/my_trip_screen.dart';
+import '../Vehicle/vehicle_request_screen.dart';
 class VehicleScreen extends StatefulWidget {
   final Map<String, dynamic> user;
   final int initialTab;
@@ -94,7 +95,7 @@ class _VehicleScreenState extends State<VehicleScreen> {
               children: [
                 VehicleRequestFormScreen(user: widget.user),
                 MyTripsScreen(user: widget.user),
-                // VehicleRequestsScreen(user: widget.user),
+                VehicleRequestScreen(),
               ],
             ),
           ),
